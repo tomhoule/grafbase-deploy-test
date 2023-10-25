3c5cc3f4be6ec7ca3d39a7858e1986ab96e0cf61
Tom Houlé <tom@tomhoule.com>
2023-10-25 15:38:11 +0200

    adjust getanswer

--- grafbase/resolvers/getAnswer.ts
+++ grafbase/resolvers/getAnswer.ts
@@ -2,7 +2,7 @@ import { Resolver } from '@grafbase/generated'
 
 const resolver: Resolver['Question.getAnswer'] = async (parent, args, { kv }, info) => {
     const { value } = await kv.get(`answers/${parent.id}`)
-    return `⚙️ Result of ${info.fieldName}: ${value}`
+    return `⚙️ Result of ${info.fieldName}: ${value} \nThat worked!`
 }
 
 export default resolver
